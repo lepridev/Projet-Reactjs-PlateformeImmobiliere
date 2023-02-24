34fb6ce68a4f534850ead7a35cccbe1d3afb84f7
LepriDev <lepridev35@gmail.com>
2023-02-24 00:33:24 +0000

    mise en forme de la barre et la liste dans le composant ListeDesOffres

--- src/components/pages/ListeDesOffres.js
+++ src/components/pages/ListeDesOffres.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./ListeDesOffres.css";
 import BarreTitre from "../layoutComponents/BarreTitre";
+import BarreRecherche from "../layoutComponents/BarreRecherche";
+import VerticalBox from "../Box/VerticalBox";
 
 const ListeDesOffres = () => {
   return (
     <div className="ListesDesOffres">
-      {/* <BarreTitre titre={"Liste des Offres"} /> */}
+      <BarreTitre titre={"Liste des Offres"} />
+      <div className="liste_et_barre">
+        <div className="barre">
+          <BarreRecherche />
+        </div>
+        <div className="liste">
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+          <VerticalBox />
+        </div>
+      </div>
     </div>
   );
 };
