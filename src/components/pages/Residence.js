5284191e7ea9a9d14c281c7a113c5a6cd5602df6
LepriDev <lepridev35@gmail.com>
2023-02-25 11:48:05 +0000

    creation du composant SharingButtons

--- src/components/pages/Residence.js
+++ src/components/pages/Residence.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Residence.css";
 import BarreRecherche from "../layoutComponents/BarreRecherche";
 import AdressAndPrice from "../singleProductComponents/AdressAndPrice";
+import SharingButtons from "../singleProductComponents/SharingButtons";
 
 const Residence = () => {
   return (
     <div className="residence_caontainer">
       <AdressAndPrice />
+      <SharingButtons />
     </div>
   );
 };
---
+++ src/components/singleProductComponents/SharingButtons.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./SharingButtons.css";
+
+const SharingButtons = () => {
+  return (
+    <div className="sharingButtons">
+      <button className="share">Share</button>
+      <button className="save">Save</button>
+    </div>
+  );
+};
+
+export default SharingButtons;
